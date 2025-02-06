8bc2eed6c99a898946a335c795b176809c554edd
Ramazan Yildiz <ry.yildizramazan@gmail.com>
2025-02-06 16:36:14 +0300

    genai added

--- routers/auth.py
+++ routers/auth.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from starlette import status
@@ -10,12 +10,14 @@ from models import User
 from passlib.context import CryptContext
 from jose import jwt, JWTError
 from datetime import timedelta, datetime, timezone
-
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter(
     prefix="/auth",
     tags=["Authentication"]
 )
+
+templates = Jinja2Templates(directory="templates")
 
 SECRET_KEY =  "57fw34yu2fd26c82g4f4j958j3fc48bdx28370gtr67"
 ALGORITHM = "HS256"
@@ -73,6 +75,20 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         return {"username": username, "id": user_id, "role": role}
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token is invalid")
+
+
+@router.get("/login-page")
+def render_login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+
+@router.get("/register-page")
+def render_register(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
+
+
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
--- routers/todo.py
+++ routers/todo.py
@@ -1,15 +1,29 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, status, Path, HTTPException
+from fastapi import APIRouter, Depends, status, Path, HTTPException, Request
 from pydantic import BaseModel, Field
+from starlette.responses import RedirectResponse
+from dotenv import load_dotenv
 from models import ToDo
 from sqlalchemy.orm import Session
 from database import SessionLocal
 from routers.auth import get_current_user
+from fastapi.templating import Jinja2Templates
+import os
+import google.generativeai as genai
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage, AIMessage
+import markdown
+from bs4 import BeautifulSoup
+
+
 
 router = APIRouter(
     prefix="/todo",
     tags=["ToDo"]
 )
+
+templates = Jinja2Templates(directory="templates")
+
 
 class ToDoRequest(BaseModel):
     title: str = Field(min_length=3)
@@ -29,6 +43,46 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
+
+def redirect_to_login():
+    redirect_response = RedirectResponse("/auth/login-page", status_code=status.HTTP_302_FOUND)
+    redirect_response.delete_cookie("access_token")
+    return redirect_response
+
+
+@router.get("/todo-page")
+async def render_todo(request: Request, db: db_dependency):
+    try:
+        user = await get_current_user(request.cookies.get("access_token"))
+        if user is None:
+            return redirect_to_login()
+        todos = db.query(ToDo).filter(ToDo.owner_id == user.get("id")).all()
+        return templates.TemplateResponse("todo.html", {"request": request, "todos": todos, "user": user})
+    except:
+        return redirect_to_login()
+
+
+@router.get("/add-todo-page")
+async def render_todo(request: Request, db: db_dependency):
+    try:
+        user = await get_current_user(request.cookies.get("access_token"))
+        if user is None:
+            return redirect_to_login()
+        return templates.TemplateResponse("todo.html", {"request": request, "user": user})
+    except:
+        return redirect_to_login()
+
+
+@router.get("/edit-todo-page/{todo_id}")
+async def render_todo(request: Request, db: db_dependency, todo_id: int):
+    try:
+        user = await get_current_user(request.cookies.get("access_token"))
+        if user is None:
+            return redirect_to_login()
+        todo = db.query(ToDo).filter(ToDo.id == todo_id).first()
+        return templates.TemplateResponse("edit-todo.html", {"request": request, "todo": todo, "user": user})
+    except:
+        return redirect_to_login()
 
 
 
@@ -51,6 +105,7 @@ async def read_by_id(user: user_dependency, db: db_dependency, todo_id: int = Pa
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_todo(user: user_dependency, db: db_dependency, todo_request: ToDoRequest):
     todo = ToDo(**todo_request.model_dump(), owner_id=user.get("id"))
+    todo.description = create_todo_with_gemini(todo_request.description)
     db.add(todo)
     db.commit()
 
@@ -85,3 +140,29 @@ async def delete_todo(user: user_dependency, db: db_dependency, todo_id: int = P
         raise HTTPException(status_code=status.HTTP_404_N0T_FOUND, detail="ToDo not found.")
     db.delete(todo)
     db.commit()
+
+
+def markdown_to_text(markdown_text: str):
+    html = markdown.markdown(markdown_text)
+    soup = BeautifulSoup(html, "html.parser")
+    text = soup.get_text()
+    return text
+
+
+
+
+def create_todo_with_gemini(todo_string: str):
+    load_dotenv()
+    genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+    llm = ChatGoogleGenerativeAI(model="gemini-pro")
+    response = llm.invoke(
+        [
+            HumanMessage(content="I will provide you a todo item to add my list. What i want you to do is to create a longer and more comprehensive description of that todo item, my next message is my todo item."),
+            HumanMessage(content=todo_string)
+        ]
+    )
+    return markdown_to_text(response.content)
+
+if __name__ == "__main__":
+
+    print(db_dependency)
